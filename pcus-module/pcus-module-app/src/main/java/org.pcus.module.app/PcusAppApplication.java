6cb610a5b5a0379ebfcd8dc77e51cc825be568c8
lijiawei1 <lijiawei_88@qq.com>
2018-09-07 09:29:44 +0800

    调试0.1

--- pcus-module/pcus-module-app/src/main/java/org.pcus.module.app/PcusAppApplication.java
+++ pcus-module/pcus-module-app/src/main/java/org.pcus.module.app/PcusAppApplication.java
@@ -1,4 +1,11 @@
 package org.pcus.module.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PcusAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PcusAppApplication.class);
+    }
 }
