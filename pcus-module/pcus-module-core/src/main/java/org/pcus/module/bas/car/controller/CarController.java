ce56b4096c2d2d2806519c0d84559b885d9b56f5
lijiawei1 <lijiawei_88@qq.com>
2018-09-13 21:17:41 +0800

    1.试一下core

--- pcus-module/pcus-module-core/src/main/java/org/pcus/module/bas/car/controller/CarController.java
+++ pcus-module/pcus-module-core/src/main/java/org/pcus/module/bas/car/controller/CarController.java
@@ -1,16 +1,27 @@
 package org.pcus.module.bas.car.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.pcus.module.bas.car.entity.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 @Controller
 @RequestMapping("/bas/car")
 public class CarController {
     @ResponseBody
     @RequestMapping("/a")
-    public Car a() {
+    public Car a(HttpServletRequest request, HttpServletResponse response) {
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String s = headerNames.nextElement();
+            System.out.println(s + " : " + request.getHeader(s));
+
+        }
         return new Car("a", "b") ;
     }
 }
