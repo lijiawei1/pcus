c9af583d01a4c5c0a7ae68aa4543d9076007ef08
lijiawei1 <lijiawei_88@qq.com>
2018-09-14 10:27:09 +0800

    1.分布式session

--- pcus-auth/src/main/java/org/pcus/auth/PcusAuthApplication.java
+++ pcus-auth/src/main/java/org/pcus/auth/PcusAuthApplication.java
@@ -3,15 +3,13 @@ package org.pcus.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 //@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 //@EnableJpaRepositories(repositoryBaseClass = WiselyRepositoryImpl.class)
-public class AuthServerApplication {
+public class PcusAuthApplication {
 
 	//@Bean(name = "auditorAware")
 	//public AuditorAware<String> auditorAware() {
@@ -19,7 +17,7 @@ public class AuthServerApplication {
 	//}
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServerApplication.class, args);
+		SpringApplication.run(PcusAuthApplication.class, args);
 	}
 
 }
